456a5003afd6ec8d9bb3a4f5f18595b707e19b7a
EfeZeybek05 <efezeybek2005@gmail.com>
2025-05-19 22:15:45 +0300

    member factory düzenlemesi

--- factories/MemberFactory.java
+++ factories/MemberFactory.java
@@ -2,18 +2,15 @@ package factories;
 
 import model.*;
 
-/*Factory design pattern kullanılarak üyeler oluşuturuluyor
-* */
 public class MemberFactory {
 
-    public static Member create(String name, int age, String gender, boolean canBorrow){
+    public static Member create(String name, int age, String gender, boolean canBorrow) {
 
-        return switch (gender.toLowerCase()) {
-            case "male", "female" -> new Member(name, age, gender, canBorrow);
-            default -> throw new IllegalArgumentException("Unknown gender: " + gender);
-        };
+        String lowerGender = gender.toLowerCase();
+        if (!lowerGender.equals("male") && !lowerGender.equals("female")) {
+            throw new IllegalArgumentException("Unknown gender: " + gender);
+        }
 
-
+        return new Member(name, age, gender, canBorrow);
     }
-
 }
